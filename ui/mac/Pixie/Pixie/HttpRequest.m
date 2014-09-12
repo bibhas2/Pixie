02a6bdea6b2519b739c3f91f51a5caf8030c5a1e
Bibhas Bhattacharya <bibhas2@gmail.com>
2014-09-11 20:21:22 -0400

    Strip out extra stuff from path for a cleaner display.

--- ui/mac/Pixie/Pixie/HttpRequest.m
+++ ui/mac/Pixie/Pixie/HttpRequest.m
@@ -15,11 +15,18 @@
     if (self != nil) {
         self.uniqueId = [NSString stringWithUTF8String:stringAsCString(req->uniqueId)];
         self.method = [NSString stringWithUTF8String:stringAsCString(req->method)];
-        self.path = [NSString stringWithUTF8String:stringAsCString(req->path)];
         self.host = [NSString stringWithUTF8String:stringAsCString(req->host)];
         self.statusMessage = req->responseStatusMessage->length == 0 ? @"In progress" :
             [NSString stringWithUTF8String:stringAsCString(req->responseStatusMessage)];
         self.statusCode = [NSString stringWithUTF8String:stringAsCString(req->responseStatusCode)];
+        //Strip out extra stuff from path
+        size_t i = 0;
+        for (i = 0; i < req->path->length; ++i) {
+            if (req->path->buffer[i] == ' ' || req->path->buffer[i] == '?') {
+                break;
+            }
+        }
+        self.path = [[NSString alloc] initWithBytes:req->path->buffer length:i encoding:NSUTF8StringEncoding];
     }
     
     return self;
